631225cc1ef1eb43d3f8a8e7f4dc24a11319b0b1
Moody <76251897+moodyhunter@users.noreply.github.com>
2021-07-02 10:53:37 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <QApplication>
 #include <QLabel>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTimer::singleShot(1000, [&]() { a.exit(); });
     QLabel l;
     l.setText("⚡️🇭🇰");
     l.show();
