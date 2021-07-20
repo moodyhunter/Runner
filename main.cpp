0bf3d02c816d11d84c72fee7333be560dd8cc893
Moody <76251897+moodyhunter@users.noreply.github.com>
2021-07-20 10:34:23 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,51 @@
-#include <QApplication>
-#include <QLabel>
-#include <QTimer>
+// fix ‘numeric_limits’ is not a member of ‘std’
+#include <limits>
+
+// Qt Headers
+#include <QCoreApplication>
+#include <QEventLoop>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QtDebug>
+
+QNetworkAccessManager networkManager;
+
+void request(const QUrl &url)
+{
+    qDebug() << "\nRequesting" << url.toString();
+
+    QNetworkReply *r = networkManager.get(QNetworkRequest{ url });
+
+    // NOTE: not all replies triggers this signal
+    QObject::connect(r, &QNetworkReply::encrypted, [r]() { qDebug() << "> encrypted, session protocol =" << r->sslConfiguration().sessionProtocol(); });
+    QObject::connect(r, &QNetworkReply::finished, [r]() { qDebug() << "==> request finished, session protocol =" << r->sslConfiguration().sessionProtocol(); });
+
+    // Wait till the request finished.
+    {
+        QEventLoop loop;
+        QObject::connect(r, &QNetworkReply::errorOccurred, &loop, &QEventLoop::quit);
+        QObject::connect(r, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        loop.exec();
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    QTimer::singleShot(1000, [&]() { a.exit(); });
-    QLabel l;
-    l.setText("⚡️🇭🇰");
-    l.show();
-    return a.exec();
+    QCoreApplication app(argc, argv);
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 1, 0)
+    qDebug() << "Backends" << QSslSocket::availableBackends();
+    qDebug() << "Current backend" << QSslSocket::activeBackend();
+#endif
+
+    //    request(QUrl{ "https://www.google.com" });
+    request(QUrl{ "https://tls13.akamai.io" });
+    //    request(QUrl{ "https://www.youtube.com" });
+    //    request(QUrl{ "https://github.com" });
+    request(QUrl{ "https://bing.com" });
+    request(QUrl{ "https://gnu.org" });
+    request(QUrl{ "https://qt.io" });
+    request(QUrl{ "https://download.qt.io" });
+
+    return 0;
 }
